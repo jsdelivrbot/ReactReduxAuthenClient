6eb085d6dde2c0decf792de4e820aa78325cc980
MrSound <sound014@gmail.com>
2017-03-24 00:43:45 +0700

    Add HOC require_auth.js

---
+++ src/components/auth/require_auth.js
@@ -0,0 +1,28 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+export default function (ComposedComponent) {
+    class Authentication extends Component {
+        static contextTypes = {
+            router: PropTypes.object
+        }
+        componentWillMount() {
+            console.log(this.props.authenticated);
+            if (!this.props.authenticated) {
+                this.context.router.push('/');
+            }
+        }
+
+        componentWillUpdate(nextProps) {
+            if (!nextProps.authenticated) {
+                this.context.router.push('/');
+            }
+        }
+
+        render() {
+            return <ComposedComponent {...this.props} />;
+        }
+    }
+
+    return connect(state => ({ authenticated: state.auth.authenticated }))(Authentication);
+}
